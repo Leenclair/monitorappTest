4db2fe052b17a848a0b8dff803a6a71a4eac8e16
경영기획실 <@10.0.20.157>
2022-12-09 14:42:46 +0900

    test

---
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootConfiguration
+@RestController
+public class HelloController {
+	@GetMapping("/")
+	public String hello() {
+		return "Hello world!";
+	}
+
+}
